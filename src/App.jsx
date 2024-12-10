4cdc1d3a4b93f7f4eb099cb291d105ee205e57ee
GalloSimone <gallosimonecz@gmail.com>
2024-12-10 16:07:10 +0100

    add import routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+
 
 function App() {
-  
-  return (
-    <>
-    
-    </>
-  )
+  return(
+    <BrowserRouter>
+    <Routes>
+     <Route />
+    </Routes>
+    </BrowserRouter>
+  );
+
 }
 
 export default App
