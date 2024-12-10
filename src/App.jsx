34c71762af20bed37c8757c40f0fd62bdd342278
GalloSimone <gallosimonecz@gmail.com>
2024-12-10 16:16:05 +0100

    add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
-
-
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage"
+import PostPage from "./pages/PostListPage"
 function App() {
   return(
     <BrowserRouter>
     <Routes>
-     <Route />
+     <Route index Component={HomePage} />
+     <Route path="/about" Component={AboutPage} />
+     <Route path="/post" Component={PostPage} />
     </Routes>
     </BrowserRouter>
   );
